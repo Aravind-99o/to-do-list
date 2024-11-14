7da10c5d4e1dde95cd9010f8899db6343db24e5f
Aravind-99o <iaravind.99o@gmail.com>
2024-11-14 19:27:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('TO DO-LIST')
 
-st.write('Hello world!')
+st.write('A to-do list program is a simple and efficient application that helps you manage your tasks and stay organized.')
